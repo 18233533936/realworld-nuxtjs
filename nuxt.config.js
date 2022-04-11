e57057fd4c82c4b35b72474f3e6aff0fcdebb081
范晓亮 <fanxiaoliang1@126.com>
2022-04-11 17:44:32 +0800

    打开端口

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,10 +51,10 @@ module.exports = {
         }
     },
 
-    // server: {
-    //   host: '0.0.0.0',
-    //   port: 3000
-    // },
+    server: {
+      host: '0.0.0.0',
+      port: 3000
+    },
 
     //注册插件
     plugins: [
